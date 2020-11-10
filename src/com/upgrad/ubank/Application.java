822d3e082be3c7f00bc7bef2520e7f508c0a29e5
Ishwar Soni <ishwar.soni@upgrad.com>
2020-11-10 14:49:13 +0530

    exception flow-scenario1

--- src/com/upgrad/ubank/Application.java
+++ src/com/upgrad/ubank/Application.java
@@ -116,9 +116,9 @@ public class Application {
 
         try {
             accountNo = Integer.parseInt(scan.nextLine());
+            System.out.println("You entered: " + accountNo);
         } catch (NumberFormatException e) {
             System.out.println("Account number should be in numeric form.");
-            return null;
         } finally {
             System.out.println("Current account no: " + accountNo);
         }
