10692f3662a2b0f9b0ba1f0bfab657970a22d27d
Ishwar Soni <ishwar.soni@upgrad.com>
2020-11-18 13:29:49 +0530

    created AccountDAOImpl class

--- src/com/upgrad/ubank/dao/AccountDAO.java
+++ src/com/upgrad/ubank/dao/AccountDAO.java
@@ -2,8 +2,10 @@ package com.upgrad.ubank.dao;
 
 import com.upgrad.ubank.dtos.Account;
 
+import java.sql.SQLException;
+
 public interface AccountDAO {
-    public Account findByAccountNo (int accountNo);
-    public Account create (Account account);
-    public Account updateBalance (Account account);
+    public Account findByAccountNo (int accountNo) throws SQLException;
+    public Account create (Account account) throws SQLException;
+    public Account updateBalance (Account account) throws SQLException;
 }
