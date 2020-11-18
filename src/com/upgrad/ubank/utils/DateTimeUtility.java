acc9aa7a903687929b9182e15fef6ff25ab98187
Ishwar Soni <ishwar.soni@upgrad.com>
2020-11-18 17:06:58 +0530

    added date time utility

---
+++ src/com/upgrad/ubank/utils/DateTimeUtility.java
@@ -0,0 +1,18 @@
+package com.upgrad.ubank.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class DateTimeUtility {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+
+        LocalTime localTime = LocalTime.now();
+        System.out.println(localTime);
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+    }
+}
