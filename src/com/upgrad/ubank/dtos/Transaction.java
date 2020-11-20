ec1d44280c32a95b97d86b8a688fcb55efcacf9e
Ishwar Soni <ishwar.soni@upgrad.com>
2020-11-20 08:35:30 +0530

    object streams demo

--- src/com/upgrad/ubank/dtos/Transaction.java
+++ src/com/upgrad/ubank/dtos/Transaction.java
@@ -1,6 +1,8 @@
 package com.upgrad.ubank.dtos;
 
-public class Transaction {
+import java.io.Serializable;
+
+public class Transaction implements Serializable {
     private int accountNo;
     private String date;
     private String action;
